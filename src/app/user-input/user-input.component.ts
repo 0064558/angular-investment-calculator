026049f8af45f8e38a2a51fc6a2cbb62b0c342fc
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-26 18:24:02 -0300

    feat: implement investment calculation logic and user input handling

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
 
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  enteredInitialInvestment: number = 0;
+  enteredAnnualInvestment: number = 0;
+  enteredExpectedInvestment: number = 5;
+  enteredDuration: number = 10;
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: this.enteredInitialInvestment,
+      annualInvestment: this.enteredAnnualInvestment,
+      expectedReturn: this.enteredExpectedInvestment,
+      duration: this.enteredDuration
+    });
+  }
 }
